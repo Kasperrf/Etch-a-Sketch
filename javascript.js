2adcb603738baa891a4277e77d30a0914d633032
Kasperrf <kasper_rf@hotmail.com>
2025-02-15 23:59:06 +0100

    Made grid system through nested loops

--- javascript.js
+++ javascript.js
@@ -1,4 +1,16 @@
-for (let i = 0; i <= 15; i++) {
-    const div+i = document.createElement("div")
+let className = ""
+let container = document.querySelector(".container")
 
+for (let r = 1; r <= 16; r++) {
+    className = `row`
+    const divRow = document.createElement("div")
+    divRow.classList.add(className)
+    for (let c = 1; c <= 16; c++) {
+        className = `col`
+        const divCol = document.createElement("div")
+        divCol.classList.add(className)
+        divRow.appendChild(divCol)
+    }
+    container.appendChild(divRow)
 }
+
