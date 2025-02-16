8e24d961a849ae069629f67783a486433365fd7c
Kasperrf <kasper_rf@hotmail.com>
2025-02-16 12:52:22 +0100

    Added grid functionality, added class to grid

--- javascript.js
+++ javascript.js
@@ -1,16 +1,45 @@
+// Empty classname to add value to later
 let className = ""
+
+// Fetching container
 let container = document.querySelector(".container")
 
-for (let r = 1; r <= 16; r++) {
-    className = `row`
-    const divRow = document.createElement("div")
-    divRow.classList.add(className)
-    for (let c = 1; c <= 16; c++) {
-        className = `col`
-        const divCol = document.createElement("div")
-        divCol.classList.add(className)
-        divRow.appendChild(divCol)
+// Button to generate new grid
+let btnGenerate = document.querySelector(".btnGenerate")
+btnGenerate.addEventListener("click", redoGrid)
+
+// Function to generate new grid
+function redoGrid(){
+    let userInput
+    while (isNaN(userInput) | userInput > 100 | userInput < 0) {
+        userInput = parseInt(prompt("Generate up to 100 grids"), 10)       
     }
-    container.appendChild(divRow)
+
+
+
+
+
 }
+
+function createGrid(nrGrids){
+    for (let r = 0; r <= nrGrids; r++) {
+        className = `row`
+        const divRow = document.createElement("div")
+        divRow.classList.add(className)
+        for (let c = 0; c <= nrGrids; c++) {
+            className = `col`
+            const divCol = document.createElement("div")
+            divCol.addEventListener("mouseenter", function(){
+                divCol.classList.add("colHover")
+            })
+            divCol.classList.add(className)
+            divRow.appendChild(divCol)
+        }
+        container.appendChild(divRow)
+    }
+} 
+
+createGrid(16)
+
+
 
